8478b174c2b2b84231c466031bb1e1ad3f393351
Sarthak Mandpe <sarthakmandpe06@gmail.com>
2026-09-09 11:50:56 +0530

    Defence Engineering Knowledge Assistant-1

--- src/ingest.py
+++ src/ingest.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
 
